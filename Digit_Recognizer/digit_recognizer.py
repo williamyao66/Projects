f122eeca7aa50a72bf6eb6d996d240e1ca345144
williamyao66 <williamyao2001@gmail.com>
2021-03-02 05:42:01 -0500

    Update

--- Digit_Recognizer/digit_recognizer.py
+++ Digit_Recognizer/digit_recognizer.py
@@ -1,10 +1,12 @@
-import numpy as np 
 import pandas as pd 
+import os 
+
 from tensorflow.keras.models import load_model
 
 
 tr = pd.read_csv('./Digit-Recognizer/train.csv')
 te = pd.read_csv('./Digit-Recognizer/test.csv')
+x_te = te.values.copy().astype('float64')
 
 final_model = load_model(os.getcwd() + '/model/')
 predictions = final_model.predict(x_te)
